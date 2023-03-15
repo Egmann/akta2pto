6c2eb5ac6432d3a69d0eb6d5b1ed9f7bc52354fe
Eggman_n <102284913+Egmann@users.noreply.github.com>
2023-03-15 19:12:43 +0100

    new proj
    jak sie zapisało, to jest tutej

---
+++ FPMSA/add.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Strona z menu pionowym</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="all.css">
+</head>
+<body>
+    <div class="menu">
+        <img id="logo" src="logo.png">
+        <ul>
+        <li><div><a href="glowna.php">Strona główna</a></div></li>
+        <li><div class="active"><p class="active">Dodaj Nowe Dane</p></div></li>
+        <li><div><a href="edit.php">Edytuj Dane</a></div></li>
+        <li><div><a href="">O nas</a></div></li>
+        <li><div><a href="">Kontakt</a></div></li>
+        </ul>
+    </div>
+    <h1>Dodaj:</h1>
+  </body>
+</html>
